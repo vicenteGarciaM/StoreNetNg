568311523c48e004c86fc4883ab7c5c46dfdd095
vicenteGarciaM <vicente.garciam89@gmail.com>
2025-02-28 00:46:16 -0600

    Login view

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  imports: [FormsModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+export class LoginComponent {
+  email: string = '';
+  password: string= '';
+
+  constructor() {}
+
+  login(){
+    console.log(this.email);
+    console.log(this.password);
+  }
+}
